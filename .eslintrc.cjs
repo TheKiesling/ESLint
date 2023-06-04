4d7de5a6edf4e9a8430f513a143e2162647212db
José Kiesling <84196494+TheKiesling@users.noreply.github.com>
2023-06-04 14:25:59 -0600

    final version

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'max-len': ['warn', { code: 120 }],
+    'react/function-component-definition': 'off',
+  },
+}
